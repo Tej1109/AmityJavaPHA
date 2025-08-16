2ee191481299d06399665aada46d93f9aa8ac5e3
Tejas Upadhyay <tejasupadhya5@gmail.com>
2025-08-16 19:18:53 +0530

    Apparently Q7 is empty cuz of me

--- src/Module1/PHA_M1_Q6.java
+++ src/Module1/PHA_M1_Q6.java
@@ -1,5 +1,13 @@
 package Module1;
-
+import java.util.Scanner;
 public class PHA_M1_Q6 {
-    public  static  void main(String[] args){}
+    public  static  void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number you want the table of : ");
+        int num  =  scanner.nextInt();
+        for (int i = 1;  i <= 10 ;i++ ){
+            System.out.printf("%d x %d = %d\n",num,i,num*i);
+        }
+        scanner.close();
+    }
 }
--- src/Module1/PHA_M1_Q7.java
+++ src/Module1/PHA_M1_Q7.java
@@ -1,4 +1,18 @@
 package Module1;
+/*
+Write a program that uses a while loop to find the sum of all even numbers between 1 and 50.
+ */
 
 public class PHA_M1_Q7 {
+    public  static  void main(String[] args){
+        int num = 1;
+        int sumEven = 0;
+        while (num <= 50) {
+            if (num % 2 == 0){
+                sumEven +=  num;
+            }
+            num++;
+        }
+        System.out.println("The sum of even numbers from 1 to 50 is : "+sumEven);
+    }
 }
